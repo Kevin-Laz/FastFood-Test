ff397daaacd1d595b14fe813fa386506107cabcc
Kevin Jhorman Liñan Ariza <kevin-laz@outlook.com>
2025-09-24 18:28:21 -0500

    Estructuración de la arquitectura base en el frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MenuListComponent } from './pages/menu/menu-list/menu-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MenuListComponent
+  }
+];
---
+++ frontend/src/app/pages/kitchen/kitchen-dashboard/kitchen-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kitchen-dashboard',
+  imports: [],
+  templateUrl: './kitchen-dashboard.component.html',
+  styleUrl: './kitchen-dashboard.component.scss'
+})
+export class KitchenDashboardComponent {
+
+}
