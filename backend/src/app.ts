e1fee497c1102b75e65082ab1388010396c8a7ca
Kevin Jhorman Liñan Ariza <kevin-laz@outlook.com>
2025-09-25 15:00:16 -0500

    Desactivar los cors para pruebas en el frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,11 @@ import { errorHandler } from "./errors/errorHandler";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 // Rutas principales /api
